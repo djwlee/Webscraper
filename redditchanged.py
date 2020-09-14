729c459dd7deb47ef0a5d2c69dcf106dbc928a82
David Lee <jeoungwoo97@gmail.com>
2020-09-14 14:44:30 -0700

    MVP done with the webscraper. I finally got the webscraper to give back a subreddit that one might be interested in. Now that this part is completed, it needs to be optimized and built upon

--- redditchanged.py
+++ redditchanged.py
@@ -10,20 +10,42 @@ import csv
 class commentor:
     commentlist = []
     commentauthor = []
+    commentauthorsub = []
     commentdictionary = {}
+    
+class frequencycounter:
+    counter = []
+    user_input = ''
+
+def output():
+        max_key = max(commentor.commentdictionary, key=commentor.commentdictionary.get)
+        print('Subreddit I want to recommend is:', max_key)
+
+def freeqcounter():
+    for subcounter in commentor.commentauthorsub:
+        user = str(subcounter)
+        if user.lower() != frequencycounter.user_input.lower():
+            frequencycounter.counter.append(user)
+        else:
+            continue
+    for i in range(len(frequencycounter.counter)):  
+        if frequencycounter.counter[i] in commentor.commentdictionary:
+            commentor.commentdictionary[frequencycounter.counter[i]] += 1
+        else:
+            commentor.commentdictionary[frequencycounter.counter[i]] = 1
 
 def find_user_profile(user_input):
     for unique_author in commentor.commentauthor:
         comment_author = str(unique_author)   
         for submission in reddit.redditor(comment_author).submissions.top("all"):
-            print(submission.subreddit)
-            
-            
+            commentor.commentauthorsub.append(submission.subreddit)
+
 
 def subreddit_scrape():
     print("Please do not put any spaces in your input")
     user_input = input("What Subreddit are you interested in? ")
     
+    frequencycounter.user_input = str(user_input)
 
     Targeted_posts = reddit.subreddit(user_input).hot(limit=10)
     for posts in Targeted_posts:
@@ -38,3 +60,5 @@ def subreddit_scrape():
 if __name__ == "__main__":
     subreddit_scrape()
     find_user_profile(subreddit_scrape)
+    freeqcounter()
+    output()
